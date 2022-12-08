4cd3a4ba458586120073a75b204ca58dc3828f31
Ruan Vitor Scherer <ruan.vscherer@gmail.com>
2022-12-08 20:30:44 -0300

    perf: increase performance with component memoization

--- src/components/ProductItem.tsx
+++ src/components/ProductItem.tsx
@@ -1,15 +1,32 @@
+import { memo } from "react";
+
 interface ProductItemProps {
   product: {
-    id: number
-    price: number
-    title: string
-  }
+    id: number;
+    price: number;
+    title: string;
+  };
 }
 
-export function ProductItem({ product }: ProductItemProps) {
+function ProductItemComponent({ product }: ProductItemProps) {
   return (
     <div>
       {product.title} - <strong>{product.price}</strong>
     </div>
-  )
+  );
 }
+
+export const ProductItem = memo(
+  ProductItemComponent,
+  (prevProps, nextProps) => {
+    return Object.is(prevProps.product, nextProps.product);
+  }
+);
+
+/**
+ * Applies to
+ * 1. Pure Functional Components
+ * 2. Components that render too often
+ * 3. Re-renders with same props
+ * 4. Components from medium to big size
+ */
